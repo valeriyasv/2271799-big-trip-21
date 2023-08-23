7c490325b28ed4c61a60f0e3465e14ac55d8617f
Валерия <94747472+valeriyasv@users.noreply.github.com>
2023-08-23 00:05:41 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import {render} from './render.js';
 import BoardPresenter from './presenter/presenter.js';
