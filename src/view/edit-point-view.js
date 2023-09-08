488b2df0f2f9d6fbec622e03fb5ae42070c5ad2f
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-09-08 01:06:06 -0500

    Исправила ошибки с типами точек

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,6 +1,6 @@
 import AbstractView from '../framework/view/abstract-view';
 
-function editPointTemplate ({type, destination, offers, price}) {
+function editPointTemplate ({type, destination, offers, price, name}) {
   return (
     ` <li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
@@ -67,7 +67,7 @@ function editPointTemplate ({type, destination, offers, price}) {
           <label class="event__label  event__type-output" for="event-destination-1">
             Flight
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${name}" list="destination-list-1">
           <datalist id="destination-list-1">
             <option value="Amsterdam"></option>
             <option value="Geneva"></option>
